fe3c2f75477085b065de073e89f81edc42f09f05
Centre Team <cwma.campbell@gmail.com>
2021-11-20 18:04:55 +0000

    Add files via upload

---
+++ test_delete.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use feature 'say';
+use autodie;
+
+my $file = '/home/alice/R/simpsons_data.csv';
+
+# build the regex in a subroutine
+sub text_parser {
+	my ($text) = @_;
+# no g modifier on the qr creation
+	my $regex = qr/homer/i;
+	if ($text =~ m/$regex/g) {
+		return say $text;
+	}
+}
+
+open (my $fh,'<',$file);
+while (my $lines = <$fh>) {
+  chomp $lines;
+  text_parser($lines);
+  }
+close $fh;
+exit;
